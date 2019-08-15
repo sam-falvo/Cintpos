3a78684e1008bd0e4ca556faab24253f424d7b59
Samuel A. Falvo II <kc5tja@arrl.net>
2019-08-15 13:17:32 -0700

    How did absolute paths get in there?

--- BCPL/cintcode/go-cintsys.sh
+++ BCPL/cintcode/go-cintsys.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export BCPL64ROOT=$(pwd)
-export BCPL64PATH=/home/kc5tja/git/Cintpos/BCPL/cintcode/cin64:/home/kc5tja/git/Cintpos/BCPL/cintcode/cin64/syscin
+export BCPL64PATH=$BCPL64ROOT/cin64:$BCPL64ROOT/cin64/syscin
 export BCPL64HDRS=$BCPL64ROOT/g
 
 export PATH=$PATH:$(pwd)/bin
