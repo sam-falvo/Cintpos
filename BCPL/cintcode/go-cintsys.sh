0df9fbbfa3e28cfbce5d50c48e4e46785da7902f
Samuel A. Falvo II <kc5tja@arrl.net>
2019-08-14 22:44:05 -0700

    Tell compiler where to find headers

--- BCPL/cintcode/go-cintsys.sh
+++ BCPL/cintcode/go-cintsys.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 export BCPL64ROOT=$(pwd)
 export BCPL64PATH=/home/kc5tja/git/Cintpos/BCPL/cintcode/cin64:/home/kc5tja/git/Cintpos/BCPL/cintcode/cin64/syscin
+export BCPL64HDRS=$BCPL64ROOT/g
+
 export PATH=$PATH:$(pwd)/bin
 
