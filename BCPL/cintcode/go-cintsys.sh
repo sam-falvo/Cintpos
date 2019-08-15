c1845513efe02567584bd5dbe3ada8fa3b4772ed
Samuel A. Falvo II <kc5tja@arrl.net>
2019-08-14 22:39:27 -0700

    Script to set needed env vars

---
+++ BCPL/cintcode/go-cintsys.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+export BCPL64ROOT=$(pwd)
+export BCPL64PATH=/home/kc5tja/git/Cintpos/BCPL/cintcode/cin64:/home/kc5tja/git/Cintpos/BCPL/cintcode/cin64/syscin
+export PATH=$PATH:$(pwd)/bin
+
